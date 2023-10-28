71e7493fa1079977e47d6fe936cb6b0f515827c7
Durotan <janfojtik@icloud.com>
2023-10-28 21:44:30 +0200

    Added game board

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,25 +3,28 @@ import java.util.*;
 
 public class TicTacToe {
 
+    static ArrayList <Integer> playerPosition= new ArrayList<Integer>();
+    static ArrayList <Integer> cpuPosition = new ArrayList<Integer>();
+
+
     static String board[] = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
 
 
     //Nakresli herni pole
     public static void drawBoard(){
-        System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | "
-                + board[1] + " | " + board[2]
-                + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | "
-                + board[4] + " | " + board[5]
-                + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | "
-                + board[7] + " | " + board[8]
-                + " |");
-        System.out.println("|---|---|---|");
+        System.out.println("--------------");
+        System.out.println(" | "+ board[0] + " | "
+                + board[1]+ " | " + board[2]
+                + " | ");
+        System.out.println(" | "+ board[3] + " | "
+                + board[4]+ " | " + board[5]
+                + " | ");
+        System.out.println(" | "+ board[6] + " | "
+                + board[7]+ " | " + board[8]
+                + " | ");
+        System.out.println("--------------");
     }
+
 
 
 
