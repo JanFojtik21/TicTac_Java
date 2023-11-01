e88fffe2d9af08e9b694dc549d2d62790c9b6a8a
Durotan <janfojtik@icloud.com>
2023-11-01 13:06:26 +0100

    Pridani game board, pridani loopu k vytisteni herniho pole a pridani switche

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,30 +3,70 @@ import java.util.*;
 
 public class TicTacToe {
 
-    static ArrayList <Integer> playerPosition= new ArrayList<Integer>();
-    static ArrayList <Integer> cpuPosition = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                             {'-', '+', '-', '+', '-'},
+                             {' ', '|', ' ', '|', ' '},
+                             {'-', '+', '-', '+', '-'},
+                             {' ', '|', ' ', '|', ' '}};
+
+            printGameBoard(gameBoard);
 
 
-    static String board[] = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your choice(1-9):\n");
+            int position = scanner.nextInt();
+            //if position is one chance first position a tak dale.
+
+            System.out.println(position);
+            switch (position){
+                case 1:
+                    gameBoard[0] [0] = 'X';
+                    break;
+                case 2:
+                    gameBoard[0] [1] = 'X';
+                    break;
+                case 3:
+                    gameBoard[0] [2] = 'X';
+                    break;
+                case 4:
+                    gameBoard[1] [0] = 'X';
+                    break;
+                case 5:
+                    gameBoard[1] [1] = 'X';
+                    break;
+                case 6:
+                    gameBoard[1] [2] = 'X';
+                    break;
+                case 7:
+                    gameBoard[2] [0] = 'X';
+                    break;
+                case 8:
+                    gameBoard[2] [1] = 'X';
+                    break;
+                case 9:
+                    gameBoard[2] [2] = 'X';
+                    break;
+            }
 
 
-    //Nakresli herni pole
-    public static void drawBoard(){
-        System.out.println("--------------");
-        System.out.println(" | "+ board[0] + " | "
-                + board[1]+ " | " + board[2]
-                + " | ");
-        System.out.println(" | "+ board[3] + " | "
-                + board[4]+ " | " + board[5]
-                + " | ");
-        System.out.println(" | "+ board[6] + " | "
-                + board[7]+ " | " + board[8]
-                + " | ");
-        System.out.println("--------------");
-    }
 
 
-    public static void main (String [] args){
-    drawBoard();
-    }
 }
+
+
+
+    public static void printGameBoard(char [] [] gameBoard){
+
+        for(char[] row:gameBoard){
+            for(char c : row){
+                System.out.print(c);
+            }
+
+            System.out.println();
+        }
+    }
+
+    }
