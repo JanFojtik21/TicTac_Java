131a3f9a9ee048aec28e6f1bb9fdbb700ff8879f
Durotan <janfojtik@icloud.com>
2023-11-02 09:47:28 +0100

    Pridani game board, pridani loopu k vytisteni herniho pole a pridani switche

--- TicTacToe.java
+++ TicTacToe.java
@@ -13,50 +13,21 @@ public class TicTacToe {
                              {' ', '|', ' ', '|', ' '}};
 
             printGameBoard(gameBoard);
-
-
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter your choice(1-9):\n");
             int position = scanner.nextInt();
             //if position is one chance first position a tak dale.
 
-            System.out.println(position);
-            switch (position){
-                case 1:
-                    gameBoard[0] [0] = 'X';
-                    break;
-                case 2:
-                    gameBoard[0] [1] = 'X';
-                    break;
-                case 3:
-                    gameBoard[0] [2] = 'X';
-                    break;
-                case 4:
-                    gameBoard[1] [0] = 'X';
-                    break;
-                case 5:
-                    gameBoard[1] [1] = 'X';
-                    break;
-                case 6:
-                    gameBoard[1] [2] = 'X';
-                    break;
-                case 7:
-                    gameBoard[2] [0] = 'X';
-                    break;
-                case 8:
-                    gameBoard[2] [1] = 'X';
-                    break;
-                case 9:
-                    gameBoard[2] [2] = 'X';
-                    break;
-            }
+            placePiece(gameBoard, position, "player");
 
+            Random rand = new Random();
+            int cpuPos = rand.nextInt((9) +1); // udelej to v rozmeni 1-9
 
+            placePiece(gameBoard, cpuPos, "cpu" );
 
+            printGameBoard(gameBoard);
 
 }
-
-
 
     public static void printGameBoard(char [] [] gameBoard){
 
@@ -69,4 +40,50 @@ public class TicTacToe {
         }
     }
 
+    public static void placePiece(char [] [] gameBoard, int position, String user){
+
+        char symbol = ' ';
+
+
+        if (user.equals("player")){
+            symbol = 'X';
+        } else if (user.equals("cpu")){
+            symbol = 'O';
+        }
+
+
+        switch (position){
+            case 1:
+                gameBoard[0] [0] = symbol;
+                break;
+            case 2:
+                gameBoard[0] [2] = symbol;
+                break;
+            case 3:
+                gameBoard[0] [4] = symbol;
+                break;
+            case 4:
+                gameBoard[2] [0] = symbol;
+                break;
+            case 5:
+                gameBoard[2] [2] = symbol;
+                break;
+            case 6:
+                gameBoard[2] [4] = symbol;
+                break;
+            case 7:
+                gameBoard[4] [0] = symbol;
+                break;
+            case 8:
+                gameBoard[4] [2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+            default:
+                break;
+        }
+
+    }
+
+    
     }
